cb56dcaa2e2a5fe2c278f77b92c50692a3ceb97b
Charles Bentein <c.bentein@rubika-edu.com>
2019-12-09 12:17:00 +0100

    Working on box script

--- ChB_B2GD2_RenduS1/Assets/CHB_Script/Boxes/Box.cs
+++ ChB_B2GD2_RenduS1/Assets/CHB_Script/Boxes/Box.cs
@@ -9,14 +9,39 @@ public class Box : MonoBehaviour
     private int iD;
     public bool needPickUp;
     public GameObject breakFrame;
+    public GameObject boxPosKeeper;
+    private BoxPos boxArray;
+    public bool isReceived;
+
     void Start()
     {
-        
+        boxArray = boxPosKeeper.GetComponent<BoxPos>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameObject.GetComponent<SpriteRenderer>().enabled)
+        {
+            if (needPickUp)
+            {
+                //Character makes transit or the box breaks
+            }
+            else
+            {
+                StartCoroutine(NormalStep(3f));
+            }
+        }
+    }
+
+    IEnumerator NormalStep(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        boxArray.boxes[iD + 1].GetComponent<SpriteRenderer>().enabled = true;
+        boxArray.boxHere[iD + 1] = true;
+
+        boxArray.boxes[iD].GetComponent<SpriteRenderer>().enabled = false;
+        boxArray.boxHere[iD] = false;
     }
 }
