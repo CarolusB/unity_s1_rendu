19f48b974d3ebd5c80043de69c0593f034a1fe77
Charles Bentein <c.bentein@rubika-edu.com>
2019-12-11 12:10:20 +0100

    Have a break, have a miss count

--- ChB_B2GD2_RenduS1/Assets/CHB_Script/Manager/MissCounter.cs
+++ ChB_B2GD2_RenduS1/Assets/CHB_Script/Manager/MissCounter.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class MissCounter : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] breakFrames = new GameObject[3];
+    private SpriteRenderer[] breakSprites = new SpriteRenderer[3];
+
+    public GameObject[] missFaces = new GameObject[3];
+    private int missCount = 0;
+    private bool coRoutineOn = false;
+
+    private void Awake()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            breakSprites[i] = breakFrames[i].GetComponent<SpriteRenderer>();
+            breakSprites[i].enabled = true;
+        }
+    }
     void Start()
     {
-        
+        for (int i = 0; i < 3; i++)
+        {
+            breakSprites[i].enabled = false;
+            missFaces[i].SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (missCount < 3)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (breakSprites[i].enabled)
+                {
+                    missFaces[missCount].SetActive(true);
+
+                    StartCoroutine(DelayedTickOff(1.2f));
+                    breakSprites[i].enabled = false;
+                    missCount++;
+
+                }
+            }
+        }
+
+        Debug.Log("Nb of misses: " + missCount);
     }
+
+    IEnumerator DelayedTickOff(float delay)
+    {
+        Time.timeScale = 0f;
+        yield return new WaitForSecondsRealtime(delay);
+        Time.timeScale = 1f;
+    }
+
+
 }
